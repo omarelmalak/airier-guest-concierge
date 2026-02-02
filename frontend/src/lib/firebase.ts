8c86c16fd2e5b324069d43f4ba73f8f1d03b8075
Omar El Malak <omarelmalak28@gmail.com>
2026-02-01 23:50:52 -0500

    Implemented fallback to default images based on propertyType

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -13,29 +13,23 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
 export async function uploadPropertyPhoto(file: File, userId: string) {
-    // Generate unique filename to avoid overwrites
     const timestamp = Date.now();
     const randomString = Math.random().toString(36).substring(2, 15);
     const fileExtension = file.name.split('.').pop();
     const uniqueFileName = `${timestamp}-${randomString}.${fileExtension}`;
-    console.log("userID:", userId);
 
-    // Path inside Firebase Storage - flat structure
     const fileRef = ref(
         storage,
         `user-property-photos/${userId}/${file.name}`
     );
 
-    // Upload
     const snapshot = await uploadBytes(fileRef, file);
 
-    // Get public URL
     const downloadUrl = await getDownloadURL(snapshot.ref);
 
     return downloadUrl;
