42c7d11ce95013c758098254ea3045d6dbd272f5
Omar El Malak <omarelmalak28@gmail.com>
2026-03-15 16:01:51 -0400

    Updated CORS config for ruby to accept new prod static site host

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -13,4 +13,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  allow do
+    origins "https://frontend-us-east.onrender.com/"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  allow do
+    origins "https://airier.app", "https://www.airier.app"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
