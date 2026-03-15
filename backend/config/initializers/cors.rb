56abe03429c75f5cbc57ffc65e6561740da690a5
Omar El Malak <omarelmalak28@gmail.com>
2026-03-15 16:05:06 -0400

    Removed trailing backslash

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
   allow do
-    origins "https://frontend-us-east.onrender.com/"
+    origins "https://frontend-us-east.onrender.com"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
